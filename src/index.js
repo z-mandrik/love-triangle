413141e9deb81c44be92b6b113d5cfbedd39cc98
mandrik.zhenya <mandrik.zhenya1@gmail.com>
2019-03-19 15:49:10 +0300

    first

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+ var count=0;
+ for(var i=0;i<preferences.length;i++){
+  var first_love=preferences[i],second_love=preferences[first_love-1],third_love=preferences[second_love-1];
+  if(third_love-1==i&&second_love!=third_love)
+      count++;}
+return count/3;
+ };
